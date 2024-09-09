97683e5698dc3fd02b0fd4d4f6ba87e7bb76ef5b
Jollepolle123 <80117628+Jollepolle123@users.noreply.github.com>
2024-09-09 17:14:41 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+//const text = document.getElementById('text')
+//console.log(text)
+//const welcome = document.getElementById('welcome')
+//welcome.innerHTML = 'Hej ' + text.value + ', här har du en söt bild på min hund:'
